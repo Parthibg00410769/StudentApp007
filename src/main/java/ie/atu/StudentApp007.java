4368bda4dad395487c5fc4b2af1cc9a5102a3717
Parthibg00410769 <g00410769@atu.ie>
2022-10-25 14:08:03 +0100

    Finally updated

--- src/main/java/ie/atu/StudentApp007.java
+++ src/main/java/ie/atu/StudentApp007.java
@@ -21,40 +21,29 @@ public class StudentApp007 {
         stuDetails.setCourse(Studentcourse);
 
         ///Second Instance
-        Studentdetails stu2Details = new Studentdetails();
+
         System.out.println("Please enter your student2 name: ");
         String Student2name = stringScanner.nextLine();
-        stu2Details.setName(Student2name);
-
-
-        //
         System.out.println("Please enter your student2 email: ");
         String Student2email = stringScanner.nextLine();
-        stu2Details.setEmail(Student2email);
-
-        //
         System.out.println("Please enter your course2 information: ");
         String Student2course = stringScanner.nextLine();
-        stu2Details.setCourse(Student2course);
+
+        Studentdetails stu2Details = new Studentdetails(Student2name);
+        stu2Details.setEmail(Student2name);
+        stu2Details.setCourse(Student2name);
 
 
         ///Third Instance
-        Studentdetails stu3Details = new Studentdetails();
+
         System.out.println("Please enter your student3 name: ");
         String Student3name = stringScanner.nextLine();
-        stu3Details.setName(Student3name);
-
-
-        //
         System.out.println("Please enter your student3 email: ");
         String Student3email = stringScanner.nextLine();
-        stu3Details.setEmail(Student3email);
-
-
-        //
         System.out.println("Please enter your course3 information: ");
         String Student3course = stringScanner.nextLine();
-        stu3Details.setCourse(Student3course);
+        Studentdetails stu3Details = new Studentdetails(Student3name, Student3email, Student3course);
+
 
         //////Printing the details////////
         System.out.println(stuDetails.getName());
